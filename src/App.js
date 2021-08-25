cdf0512b3747dc6733baea217f429fcf8f4cd179
polinello <nelloxpolidoro@gmail.com>
2021-08-25 10:29:21 +0200

    Components part2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
 
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
