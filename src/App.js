b04697580079c04776a31361dc989b04d8c0d099
polinello <npolidoro@sogei.it>
2021-08-27 11:46:01 +0200

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import './App.css';
-import Main from './components/MainComponent';
-import { BrowserRouter } from 'react-router-dom';
-import { DISHES } from './shared/dishes';
-import React from 'react';
+import "./App.css";
+import Main from "./components/MainComponent";
+import { BrowserRouter } from "react-router-dom";
+import { DISHES } from "./shared/dishes";
+import React from "react";
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+
+const store = ConfigureStore();
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
     };
   }
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
